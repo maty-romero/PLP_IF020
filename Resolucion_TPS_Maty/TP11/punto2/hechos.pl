1ed289f34e55b113fe165cd85aeee36dae2a98bb
Hans <ich.arbeite.rd@gmail.com>
2025-10-20 10:47:47 -0300

    p2 sin terminar

---
+++ Resolucion_TPS_Maty/TP11/punto2/hechos.pl
@@ -0,0 +1,19 @@
+/* 
+Representar las siguientes sentencias en notación Prolog
+
+a) a juan le gusta el helado.
+b) jose es alto.
+c) juan viaja a comodoro en auto.
+d) juan come solamente chocolate, manzanas o queso.
+e) la raiz cuadrada de 16 es 4 o -4.
+f) argentina, uruguay y peru son paises.
+*/
+
+% a) a juan le gusta el helado.
+gusta('juan', 'helado').
+% b) jose es alto.
+es_alto('jose')
+% c) juan viaja a comodoro en auto.
+viaja_en_auto('juan', 'comodoro')
+% d) juan come solamente chocolate, manzanas o queso.
+come('juan')
